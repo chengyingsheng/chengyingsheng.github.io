5551e5409f2d06cacf5353e0c249182c218e3769
luoGuangHui <guanghui.luo@foxmail.com>
2025-03-17 09:51:32 +0800

    feat:d

--- sites.js
+++ sites.js
@@ -23,4 +23,4 @@ panel.append(getSiteContent("Google Pay","https://play.google.com/console/u/0/de
 panel.append(getSiteContent("Crowdin","https://wejoy.crowdin.com/u/projects/11/files", "favicons/crowdin.ico"));
 panel.append(getSiteContent("微派图床","https://config-admin.weplayapp.com/global/tools/compression", "favicons/wepie.ico"));
 panel.append(getSiteContent("TinyPNG","https://tinify.cn/", "favicons/tinypng.ico"));
-panel.append(getSiteContent("飞书文档","https://wepie.feishu.cn/wiki/"));
+// panel.append(getSiteContent("飞书文档","https://wepie.feishu.cn/wiki/"));
