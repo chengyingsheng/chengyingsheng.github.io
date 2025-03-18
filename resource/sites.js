3ca572eb291e88efa7f3fcbce45dd5b8b79661cd
luoGuangHui <guanghui.luo@foxmail.com>
2025-03-18 08:51:08 +0800

    feat:3

--- resource/sites.js
+++ resource/sites.js
@@ -8,19 +8,19 @@ function getSiteContent(name,link,icon) {
 
 var panel = $("#sites_panel");
 console.log(panel.text());
-panel.append(getSiteContent("Github","https://github.com/","favicons/github.png"));
-panel.append(getSiteContent("小破站","https://www.bilibili.com/","favicons/bilibili.png"));
-panel.append(getSiteContent("字幕库","http://zimuku.org/"));
-panel.append(getSiteContent("码云","https://gitee.com/peanutbutter/dashboard/projects","favicons/mayun.ico"));
-panel.append(getSiteContent("BTBTT","https://btbtt11.com/","favicons/btbtt.ico"));
-panel.append(getSiteContent("在线正则","https://regex101.com/","favicons/regex101.ico"));
-panel.append(getSiteContent("俄区正版游戏","https://byrut.org/index.php?do=","favicons/byrut.png"));
-panel.append(getSiteContent("飞书项目","https://project.feishu.cn/"));
-panel.append(getSiteContent("GitLab","https://git.17zjh.com/", "favicons/gitlab.png"));
-panel.append(getSiteContent("Z-Library","https://zlibrary-ca.se/", "favicons/z.ico"));
-panel.append(getSiteContent("Firebase","https://console.firebase.google.com/project/weplay-1615777951368/overview?hl=zh-cn", "favicons/firebase_28dp.png"));
-panel.append(getSiteContent("Google Pay","https://play.google.com/console/u/0/developers/5129658753235517501/app-list", "favicons/google.ico"));
-panel.append(getSiteContent("Crowdin","https://wejoy.crowdin.com/u/projects/11/files", "favicons/crowdin.ico"));
-panel.append(getSiteContent("微派图床","https://config-admin.weplayapp.com/global/tools/compression", "favicons/wepie.ico"));
-panel.append(getSiteContent("TinyPNG","https://tinify.cn/", "favicons/tinypng.ico"));
+panel.append(getSiteContent("Github","https://github.com/chengyingsheng","favicons/github.png"));
+panel.append(getSiteContent("代码","http://123.60.208.206:90/", "favicons/gitlab.png"));
+panel.append(getSiteContent("翻译","https://fanyi.baidu.com/mtpe-individual/multimodal#/","https://fanyi.baidu.com/favicon.ico"));
+panel.append(getSiteContent("码云","https://gitee.com/","https://gitee.com/favicon.ico"));
+// panel.append(getSiteContent("BTBTT","https://btbtt11.com/","favicons/btbtt.ico"));
+// panel.append(getSiteContent("在线正则","https://regex101.com/","favicons/regex101.ico"));
+// panel.append(getSiteContent("俄区正版游戏","https://byrut.org/index.php?do=","favicons/byrut.png"));
+// panel.append(getSiteContent("飞书项目","https://project.feishu.cn/"));
+
+// panel.append(getSiteContent("Z-Library","https://zlibrary-ca.se/", "favicons/z.ico"));
+// panel.append(getSiteContent("Firebase","https://console.firebase.google.com/project/weplay-1615777951368/overview?hl=zh-cn", "favicons/firebase_28dp.png"));
+// panel.append(getSiteContent("Google Pay","https://play.google.com/console/u/0/developers/5129658753235517501/app-list", "favicons/google.ico"));
+// panel.append(getSiteContent("Crowdin","https://wejoy.crowdin.com/u/projects/11/files", "favicons/crowdin.ico"));
+// panel.append(getSiteContent("微派图床","https://config-admin.weplayapp.com/global/tools/compression", "favicons/wepie.ico"));
+// panel.append(getSiteContent("TinyPNG","https://tinify.cn/", "favicons/tinypng.ico"));
 // panel.append(getSiteContent("飞书文档","https://wepie.feishu.cn/wiki/"));
